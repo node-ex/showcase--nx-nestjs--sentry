5dcd993dc0f16db24afcb7cc5c8a893e7bd15434
Pavel Balashov <pavel.balashov1@gmail.com>
2025-01-13 21:07:37 +0100

    chore(app-nest-1): add a showcase comment to the GlobalCatchAllExceptionFilter code

--- apps/app-nest-1/src/app/global-catch-all.exception-filter.ts
+++ apps/app-nest-1/src/app/global-catch-all.exception-filter.ts
@@ -1,6 +1,7 @@
 import { ArgumentsHost, Catch } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { SentryExceptionCaptured } from '@sentry/nestjs';
+import * as Sentry from '@sentry/nestjs';
 
 /**
  * Copy-pasted from:
@@ -10,6 +11,8 @@ import { SentryExceptionCaptured } from '@sentry/nestjs';
 export class GlobalCatchAllExceptionFilter extends BaseExceptionFilter {
   @SentryExceptionCaptured()
   override catch(exception: unknown, host: ArgumentsHost): void {
+    /* Use this if you want to send exceptions to Sentry manually */
+    // Sentry.captureException(exception);
     super.catch(exception, host);
   }
 }
